efc9f2db163c78864f9fa1a05a384b70c7a91c0f
georgistoilov8 <gogo.stoilov@abv.bg>
2016-03-21 19:30:36 +0200

    Add Web Version

---
+++ Web_Darts/account.php
@@ -0,0 +1,21 @@
+<?php include_once "common/header.php"; ?>
+
+<div id="main">
+
+   <!-- IF LOGGED IN -->
+
+          <!-- Content here -->
+
+   <!-- IF LOGGED OUT -->
+
+          <!-- Alternate content here -->
+
+</div>
+
+<?php
+
+    include_once "common/sidebar.php";
+
+    include_once "common/footer.php";
+
+?>
---
+++ Web_Darts/common/loggedout.php
@@ -0,0 +1,15 @@
+<ul id="list">
+   <li class="colorRed">
+        <span>Walk the dog</span>
+    </li>
+
+    <li class="colorBlue">
+        <span>Pick up dry cleaning</span>
+    </li>
+
+    <li class="colorGreen">
+        <span>Milk</span>
+    </li>
+</ul>
+
+<img src="/images/newlist.jpg" alt="Your new list here!" />
