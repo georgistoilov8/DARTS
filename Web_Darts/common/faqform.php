dbcedfc41b2f74ffcac7b904db693f3ebe3f0a03
ivo <ivo@ivo-server>
2016-06-06 01:39:23 +0300

    Adding updated FAQ

--- Web_Darts/common/faqform.php
+++ Web_Darts/common/faqform.php
@@ -32,7 +32,16 @@
           </div>
           <div id="collapse-2" class="panel-collapse collapse">
             <div class="panel-body">
-              An awesome darts scoreboard and statistics app for the web.
+              	An awesome darts scoreboard and statistics app for the web. It's basically a website
+		that has a scoreboard for the popular pub game of darts. It currently supports the standard
+		301, 501, etc. format, but will support other gamemodes(and possibly other games) as development 
+		of the site progresses. It also supports statistics tracking, where it calculates statistics such 
+		as wins, losses, 3-dart average, checkout percenteges etc. The goal is to generate a leaderboard
+		for all users of the site. In the future we will also add progress tracking, where players will 
+		have access to progression graphs that shows them their stats overtime, which lets them see whether their
+		game is improving or not. This will allow them to figure out what element of their game to focus on during 
+		training.<br>
+		In the more distant future, scoreboard and statistics support may be added for other games and sports.  
             </div>
           </div>
         </div>
@@ -47,7 +56,13 @@
           </div>
           <div id="collapse-3" class="panel-collapse collapse">
             <div class="panel-body">
-              Because we can track all the stats you need to enhance your game, and our scoreboard is pretty!!!
+            	Currently our scoreboard supports the standard darts gamemode
+ 		of playing from N hundred and one down to zero ( i.e. 301, 501). However we are hard at work on
+		implementing support for other darts variations such as Around the Clock, Tic Tac Toe and others.<br>
+ 		Another key feature is stat tracking. After researching other darts scoring apps, we have concluded that
+		we track the most stats of any other app. Also there does not seem to be overtime stat tracking in any of
+		them (research not 100% conclusive). We would also be one of the first to support a real-time leaderboard
+		for all registered players
             </div>
           </div>
         </div>
@@ -57,10 +72,63 @@
         <div class="panel-heading">
           <div class="panel-title">
             <a href="#collapse-4" data-toggle="collapse" data-parent="#accordion">
-              Our introduction video.
+              Do I have to pay for any of this?
             </a>
           </div>
           <div id="collapse-4" class="panel-collapse collapse">
+            <div class="panel-body">
+            	The features of the website will be free to all registered users. Registration is free of charge.
+		A desktop version of the entire service will be available soon. That version will be available for 
+		purchase, price is yet to be determined. The desktop version will offer offline support. This means
+		you will not require a DartsPro2016 profile in order to use the app, rather you will have local profiles
+		with which you will track your stats. The desktop app will have a local leaderboard spanning all local profiles.
+            </div>
+          </div>
+        </div>
+      </div>
+	
+	<div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="panel-title">
+            <a href="#collapse-5" data-toggle="collapse" data-parent="#accordion">
+              Why would i want the desktop version?
+            </a>
+          </div>
+          <div id="collapse-5" class="panel-collapse collapse">
+            <div class="panel-body">
+            	The desktop version is ideal for darts clubs big or small, where you would want a localized leaderboard
+		between the club members alone. It's also preferable for people with less of a stable internet connection
+		(as uncommon as that may be).
+            </div>
+          </div>
+        </div>
+      </div>
+	
+	<div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="panel-title">
+            <a href="#collapse-6" data-toggle="collapse" data-parent="#accordion">
+              Will this service be on mobile?
+            </a>
+          </div>
+          <div id="collapse-6" class="panel-collapse collapse">
+            <div class="panel-body">
+            	The web version of DartsPro2016 is fully compatible with mobile devices of all screen sizes. However
+		a standalone mobile version is in development, which will offer simmilar features to the desktop version,
+		including offline usability and a local leaderboard.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <div class="panel-title">
+            <a href="#collapse-7" data-toggle="collapse" data-parent="#accordion">
+              Our introduction video.
+            </a>
+          </div>
+          <div id="collapse-7" class="panel-collapse collapse">
             <div class="panel-body">
               
             </div>
@@ -68,6 +136,7 @@
         </div>
       </div>
     </div>
-
+	
+	 
   </section>
 </div>
